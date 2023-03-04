28402c852969f38d627b4b04aa5716b485584c29
최진우 <cjw_git@naver.com>
2023-03-04 17:50:06 +0900

    feat : search design

--- android/app/src/main/java/com/ssafy/forpawchain/viewmodel/fragment/HouseFragmentVM.kt
+++ android/app/src/main/java/com/ssafy/forpawchain/viewmodel/fragment/HouseFragmentVM.kt
@@ -9,5 +9,5 @@ class HouseFragmentVM : ViewModel() {
         const val TAG: String = "HouseFragmentVM"
     }
 
-    val message = MutableLiveData<String>("하우스 프래그먼트입니다!")
+    val searchEditText = MutableLiveData<String>()
 }
