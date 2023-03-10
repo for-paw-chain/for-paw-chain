3b925c307ee9f9fd929301d8502af5a4a01c267e
hjle2 <02q09q@naver.com>
2023-03-10 16:00:45 +0900

    jpa entity

--- backend/src/main/java/com/forpawchain/domain/entity/PetRegEntity.java
+++ backend/src/main/java/com/forpawchain/domain/entity/PetRegEntity.java
@@ -1,10 +1,30 @@
 package com.forpawchain.domain.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PET_REG")
+@Builder
+@Getter
 public class PetRegEntity {
+    @Id
     private String pid;
+    @Column(nullable = false)
     private String sex;
+    @Column(nullable = false)
     private boolean spayed;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String type;
     private String kind;
+
+    @OneToOne
+    @JoinColumn(name = "pid")
+    @MapsId("pid")
+    private PetEntity pet;
+
 }
