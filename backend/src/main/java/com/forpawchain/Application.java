45ac019e6de069e9ef88c850b4a498775b6de20c
윤혜진 <hjyoon0223@gmail.com>
2023-03-16 16:55:43 +0900

    chore: test 주석 처리

--- backend/src/main/java/com/forpawchain/Application.java
+++ backend/src/main/java/com/forpawchain/Application.java
@@ -21,9 +21,9 @@ public class Application {
 
 	}
 
-	@Bean
-	public TestDataInit testDataInit(DoctorLicenseRepository doctorLicenseRepository, PetRepository petRepository,
-		UserRepository userRepository, AdoptRepository adoptRepository, PetRegRepository petRegRepository, PetInfoRepository petInfoRepository) {
-		return new TestDataInit(doctorLicenseRepository, petRepository, userRepository, adoptRepository, petRegRepository, petInfoRepository);
-	}
+	// @Bean
+	// public TestDataInit testDataInit(DoctorLicenseRepository doctorLicenseRepository, PetRepository petRepository,
+	// 	UserRepository userRepository, AdoptRepository adoptRepository, PetRegRepository petRegRepository, PetInfoRepository petInfoRepository) {
+	// 	return new TestDataInit(doctorLicenseRepository, petRepository, userRepository, adoptRepository, petRegRepository, petInfoRepository);
+	// }
 }
