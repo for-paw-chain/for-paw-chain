fb71ce899d602a564fded9c67ebb45edbffc6f99
hjle2 <02q09q@naver.com>
2023-03-16 16:44:58 +0900

    ipfs api test

---
+++ backend/src/main/java/com/forpawchain/controller/IPFSController.java
@@ -0,0 +1,30 @@
+package com.forpawchain.controller;
+
+import com.forpawchain.service.IPFSFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class IPFSController {
+    final private IPFSFileService ipfsService;
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestPart MultipartFile file) {
+        MultipartFile tmpfile = file;
+        return ipfsService.saveFile(tmpfile);
+    }
+
+    @GetMapping("/file/{hash}")
+    public ResponseEntity<byte[]> loadFile(@PathVariable("hash")String hash) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-type", MediaType.ALL_VALUE);
+        byte[] bytes = ipfsService.loadFile(hash);
+        return ResponseEntity.status(HttpStatus.OK).headers(httpHeaders).body(bytes);
+    }
+}
---
+++ backend/src/main/java/com/forpawchain/service/IPFSFileServiceImpl.java
@@ -0,0 +1,58 @@
+package com.forpawchain.service;
+
+import com.forpawchain.config.IPFSConfig;
+import io.ipfs.api.IPFS;
+import io.ipfs.api.MerkleNode;
+import io.ipfs.api.NamedStreamable;
+import io.ipfs.multihash.Multihash;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+@RequiredArgsConstructor
+public class IPFSFileServiceImpl implements IPFSFileService {
+
+    final private IPFSConfig ipfsConfig;
+    @Override
+    public String saveFile(MultipartFile file) {
+        try {
+            InputStream stream = new ByteArrayInputStream(file.getBytes());
+            NamedStreamable.InputStreamWrapper inputStreamWrapper = new NamedStreamable.InputStreamWrapper(stream);
+
+            MerkleNode merkleNode = ipfsConfig.ipfs.add(inputStreamWrapper).get(0);
+
+            return merkleNode.hash.toBase58();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] loadFile(String hash) {
+        Multihash filePointer = Multihash.fromBase58(hash);
+        try {
+            return ipfsConfig.ipfs.cat(filePointer);
+        } catch (IOException e) {
+            throw new RuntimeException("Error whilst communication wit the IPFS node", e);
+        }
+    }
+//    public MultipartFile convertByteArrayToMultipartFile(String fileName, byte[] fileBytes) throws IOException {
+//        // 파일 이름이나 바이트 배열이 null이거나 빈 문자열이면 MultipartFile을 생성할 수 없습니다.
+//        if(StringUtils.isEmpty(fileName) || fileBytes == null || fileBytes.length == 0) {
+//            return null;
+//        }
+//
+//        // 바이트 배열을 사용하여 MultipartFile 생성
+//        MultipartFile multipartFile = new MockMultipartFile(fileName, fileName,
+//                ContentType.APPLICATION_OCTET_STREAM.toString(), new ByteArrayInputStream(fileBytes));
+//
+//        return multipartFile;
+//    }
+}
