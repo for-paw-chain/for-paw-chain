c117a8f5512307ba91104b82272ce1fcecd6c0b2
hjle2 <02q09q@naver.com>
2023-03-21 15:51:38 +0900

    add: aop logging

--- backend/src/main/java/com/forpawchain/aop/LoggingAspect.java
+++ backend/src/main/java/com/forpawchain/aop/LoggingAspect.java
@@ -3,16 +3,25 @@ package com.forpawchain.aop;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
-    @Before("execution(* com.forpawchain...*(..))")
-    public void somethingBefore(ProceedingJoinPoint pjp) throws Throwable{
-        log.info(">>> ");
+    @Before(value="execution(* com.forpawchain.controller.*.*(..))")
+    public void loggingBefore(JoinPoint joinPoint) throws Throwable{
+        log.info(">>>>>> Called  : {}", joinPoint.getSignature().getName());
+        log.info(">>>>>> Param : {}", Arrays.toString(joinPoint.getArgs()));
+    }
+    @After(value="execution(* com.forpawchain..*.*.*(..))")
+    public void loggingAfter(JoinPoint joinPoint) throws Throwable{
+        log.info(">>>>>> End  : {}", joinPoint.getSignature().getName());
     }
 }
