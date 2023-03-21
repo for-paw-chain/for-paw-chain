c15c05482d9e1d2b62d8c10f909516356d783cde
hjle2 <02q09q@naver.com>
2023-03-21 10:52:14 +0900

    add: athentication api

--- backend/src/main/java/com/forpawchain/domain/entity/AuthenticationId.java
+++ backend/src/main/java/com/forpawchain/domain/entity/AuthenticationId.java
@@ -1,12 +1,14 @@
 package com.forpawchain.domain.entity;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @Embeddable
 public class AuthenticationId implements Serializable {
     private long uid;
