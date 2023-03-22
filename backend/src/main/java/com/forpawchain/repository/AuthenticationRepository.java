2f3b64e7b05f9931291b309071de4b2f55098343
214 <nalee9801@gmail.com>
2023-03-22 15:57:06 +0900

    fix: import 수정

--- backend/src/main/java/com/forpawchain/repository/AuthenticationRepository.java
+++ backend/src/main/java/com/forpawchain/repository/AuthenticationRepository.java
@@ -4,6 +4,8 @@ import com.forpawchain.domain.Entity.AuthenticationEntity;
 import com.forpawchain.domain.Entity.AuthenticationId;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
