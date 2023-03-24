3b482c1dd766e1932e48603ee712d608909b429f
hjle2 <02q09q@naver.com>
2023-03-24 16:03:31 +0900

    fix: ipfs return type 변경

--- backend/src/main/java/com/forpawchain/controller/IPFSController.java
+++ backend/src/main/java/com/forpawchain/controller/IPFSController.java
@@ -15,9 +15,9 @@ public class IPFSController {
     final private IPFSFileService ipfsService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> saveFile(@RequestPart MultipartFile file) {
+    public String saveFile(@RequestPart MultipartFile file) {
         MultipartFile tmpfile = file;
-        return ResponseEntity.ok(ipfsService.saveFile(tmpfile));
+        return ipfsService.saveFile(tmpfile);
     }
 
     @GetMapping("/file/{hash}")
