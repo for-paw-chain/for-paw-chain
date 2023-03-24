eea91503c3f5aaa516e67becb6ac32d955e9a6d0
hjle2 <02q09q@naver.com>
2023-03-24 14:04:35 +0900

    fix: ipfs error

--- backend/src/main/java/com/forpawchain/config/IPFSConfig.java
+++ backend/src/main/java/com/forpawchain/config/IPFSConfig.java
@@ -1,9 +1,9 @@
 package com.forpawchain.config;
 
+import com.forpawchain.exception.BaseException;
+import com.forpawchain.exception.ErrorMessage;
 import io.ipfs.api.IPFS;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class IPFSConfig {
--- backend/src/main/java/com/forpawchain/exception/ErrorMessage.java
+++ backend/src/main/java/com/forpawchain/exception/ErrorMessage.java
@@ -8,6 +8,7 @@ public enum ErrorMessage {
 	// BINDING_FAIL_EXCEPTION(1, "내부 서버에서 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 	//
 	NOT_PERMISSION_EXCEPTION(5, "권한이 없거나 부족합니다.", HttpStatus.FORBIDDEN),
+	IPFS_CONNECTION_EXCEPTION(11, "IPFS연결이 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 	//
 	// REQUEST_EXCEPTION(10, "", HttpStatus.BAD_REQUEST),
 	NOT_EXIST_CONTENT(20, "존재하지 않는 컨텐츠입니다.", HttpStatus.BAD_REQUEST),
