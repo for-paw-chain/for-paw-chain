dc48f92027fed56d2620008e794aeedfaa9cf79d
hjle2 <02q09q@naver.com>
2023-03-24 14:08:15 +0900

    fix: ipfs error

--- backend/src/main/java/com/forpawchain/config/IPFSConfig.java
+++ backend/src/main/java/com/forpawchain/config/IPFSConfig.java
@@ -16,11 +16,12 @@ public class IPFSConfig {
             return ipfs;
         } else {
             try {
-                ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
-//                ipfs = new IPFS("/ip4/3.39.235.238/tcp/5001");
+//                ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
+                ipfs = new IPFS("/ip4/3.39.235.238/tcp/5001");
 //                ipfs = new IPFS("/ip4/70.12.247.105/tcp/5001");
                 return ipfs;
             } catch (Exception e) {
+                e.printStackTrace();
                 throw new BaseException(ErrorMessage.IPFS_CONNECTION_EXCEPTION);
             }
         }
