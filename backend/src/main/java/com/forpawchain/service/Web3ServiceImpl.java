02a3d98a8652ded6f9281666d40680106b3cba57
wnsdk <jn307742@gmail.coom>
2023-03-25 16:05:43 +0900

    fix: 지갑 생성 폴더 위치 변경

--- backend/src/main/java/com/forpawchain/service/Web3ServiceImpl.java
+++ backend/src/main/java/com/forpawchain/service/Web3ServiceImpl.java
@@ -160,12 +160,12 @@ public class Web3ServiceImpl implements Web3Service {
 			// 비밀번호를 이용해 새 지갑 파일을 생성
 			String password = "1234";
 			String fileName = WalletUtils.generateNewWalletFile(password,
-				new File("\\C:\\Users\\SSAFY\\Desktop\\wallet"));
-				// new File("\\home\\ubuntu\\dev\\eth\\keystore"));
+//				new File("\\C:\\Users\\SSAFY\\Desktop\\wallet"));
+				 new File("\\home\\ubuntu\\dev\\eth\\keystore"));
 
 			// 비밀번호를 이용해 파일로부터 지갑을 로드해오기
-			String walletFilePath = "C:\\Users\\SSAFY\\Desktop\\wallet\\" + fileName;
-			// String walletFilePath = "\\home\\ubuntu\\dev\\eth\\keystore" + fileName;
+//			String walletFilePath = "C:\\Users\\SSAFY\\Desktop\\wallet\\" + fileName;
+			 String walletFilePath = "\\home\\ubuntu\\dev\\eth\\keystore" + fileName;
 
 			Credentials myCredentials = WalletUtils.loadCredentials(password, walletFilePath);
 
