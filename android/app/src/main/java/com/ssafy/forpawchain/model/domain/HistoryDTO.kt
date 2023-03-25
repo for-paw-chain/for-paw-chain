f94fb0bb029e45e57e9b0dfb1e14ed6acce060c9
최진우 <cjw_git@naver.com>
2023-03-26 05:30:32 +0900

    feat: contract 진료내역 가져오기

---
+++ android/app/src/main/java/com/ssafy/forpawchain/model/domain/HistoryDTO.kt
@@ -0,0 +1,14 @@
+package com.ssafy.forpawchain.model.domain
+
+data class Data(
+    val title: String,
+    val body: String
+)
+
+data class HistoryDTO(
+    val title: String,
+    val body: String,
+    val extra: ArrayList<Data>,
+    val writer: String,
+    val hash: String
+)
