b361cfc17772307ccde81a9210da48c220b0e651
윤혜진 <hjyoon0223@gmail.com>
2023-03-31 10:25:38 +0900

    fix : getCurrentUserInfo 호출 삭제

--- backend/src/main/java/com/forpawchain/controller/AuthenticationController.java
+++ backend/src/main/java/com/forpawchain/controller/AuthenticationController.java
@@ -27,8 +27,9 @@ public class AuthenticationController {
     @ApiOperation(value = "친구 권한 주기", notes = "요청한 동물과 사용자 사이에 친구 권한을 준다.")
     public ResponseEntity<?> giveFriendAuthentication(long receiver, String pid) {
         try {
-            UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
-            authService.giveFriendAuthentication(userInfoResDto.getUid(), receiver, pid);
+            // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+            // authService.giveFriendAuthentication(userInfoResDto.getUid(), receiver, pid);
+            authService.giveFriendAuthentication(1L, receiver, pid);
 
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
@@ -39,8 +40,9 @@ public class AuthenticationController {
     @ApiOperation(value = "권한 삭제", notes = "요청한 동물과 사용자 사이의 관계를 끊는다.")
     public ResponseEntity<?> removeAuthentication(String pid) {
         try {
-            UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
-            authService.removeAuthentication(userInfoResDto.getUid(), pid);
+            // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+            // authService.removeAuthentication(userInfoResDto.getUid(), pid);
+            authService.removeAuthentication(1L, pid);
 
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
@@ -52,8 +54,9 @@ public class AuthenticationController {
     @ApiOperation(value = "모든 권한 조회", notes = "요청한 동물에 권한을 갖고 있는 사용자 목록을 불러온다.")
     public ResponseEntity<?> getAllAuthenicatedUser(String pid) {
         try {
-            UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
-            List<UserResDto> userList = authService.getAllAuthenicatedUser(userInfoResDto.getUid(), pid);
+            // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+            // List<UserResDto> userList = authService.getAllAuthenicatedUser(userInfoResDto.getUid(), pid);
+            List<UserResDto> userList = authService.getAllAuthenicatedUser(1L, pid);
 
             HashMap<String, List> map = new HashMap<>();
             map.put("content", userList);
@@ -68,8 +71,10 @@ public class AuthenticationController {
     @ApiOperation(value = "주인 권한 주기", notes = "요청한 동물과 사용자 사이에 주인 권한을 준다.")
     public ResponseEntity<?> giveMasterAuthentication(long receiver, String pid) {
         try {
-            UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
-            authService.giveMasterAuthentication(userInfoResDto.getUid(), receiver, pid);
+            // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+            // authService.giveMasterAuthentication(userInfoResDto.getUid(), receiver, pid);
+            authService.giveMasterAuthentication(1L, receiver, pid);
+
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
@@ -79,10 +84,11 @@ public class AuthenticationController {
     @GetMapping("/pet")
     @ApiOperation(value = "동물에 대한 권한 조회", notes = "어떤 동물에 대해서, 내가 어떤 권한을 갖고 있는지 조회한다.")
     public ResponseEntity<HashMap<String, String>> getAuthenticationOfPid(String pid) {
-        UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+        // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
         HashMap<String, String> map = new HashMap<>();
 
-        AuthenticationType authenticationType = authService.getAuthenticationOfPid(userInfoResDto.getUid(), pid);
+        // AuthenticationType authenticationType = authService.getAuthenticationOfPid(userInfoResDto.getUid(), pid);
+        AuthenticationType authenticationType = authService.getAuthenticationOfPid(1L, pid);
         map.put("content", authenticationType.toString());
 
         return new ResponseEntity<>(map, HttpStatus.OK);
@@ -91,10 +97,11 @@ public class AuthenticationController {
     @GetMapping("/date")
     @ApiOperation(value = "동물과 함께한 날 조회", notes = "어떤 동물과 함께하기 시작한 날짜를 조회한다.")
     public ResponseEntity<HashMap<String, String>> getRegDate(String pid) {
-        UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
+        // UserInfoResDto userInfoResDto = userController.getCurrentUserInfo();
         HashMap<String, String> map = new HashMap<>();
 
-        String regDate = authService.getRegDate(userInfoResDto.getUid(), pid);
+        // String regDate = authService.getRegDate(userInfoResDto.getUid(), pid);
+        String regDate = authService.getRegDate(1L, pid);
         map.put("content", regDate);
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
